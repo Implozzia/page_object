8a12c3128e6f426848be2d7161c89aab92f84c32
ruslan_vaf <ruslan.vafin.93@gmail.com>
2021-02-18 11:41:20 +0300

    add marks for pytest, fix requirements

--- test_product_page.py
+++ test_product_page.py
@@ -12,6 +12,7 @@ def test_guest_cant_see_success_message(browser):
     page.should_not_be_success_message()
 
 
+@pytest.mark.need_review
 @pytest.mark.parametrize('links',
                          ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                           "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
@@ -32,6 +33,14 @@ def test_guest_can_add_product_to_basket(browser, links):
     page.add_to_basket()
     page.solve_quiz_and_get_code()
     page.should_be_equal_url()
+
+
+@pytest.mark.need_review
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
 
 
 @pytest.mark.xfail
@@ -60,6 +69,7 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.should_be_login_link()
 
 
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = 'http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/'
     page = BasketPage(browser, link)
@@ -89,6 +99,7 @@ class TestUserAddToBasketFromProductPage:
         page.open()
         page.should_not_be_success_message()
 
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/"
         page = ProductPage(browser, link)
